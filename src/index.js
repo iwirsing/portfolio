62c9a40ffab0195ab749cf38223bd5bdebfe773c
iwirsing <ivymolina@gmail.com>
2023-02-21 23:27:51 -0600

    deprecate react version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 

38740daf1b86d76e071794badf8eddeb81bbe22c
iwirsing <ivymolina@gmail.com>
2023-02-27 10:31:55 -0600

    change json setting to deploy to gihub react'

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.module.css';
 function Header(props)
 {
 
-return <div className='title' alt='IW' style={{ height: "25vh"}}>
+return <div className='title' alt='IW' style={{ height: "25vh", fontSize:"11rem"}}>
     <h1>
         {props.title}
     </h1>
